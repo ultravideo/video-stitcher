939381d85ecaa04fa3fa0f02f94418b867161ad4
Aaro Altonen <aaro.altonen@tut.fi>
2018-10-30 09:18:48 +0200

    rewrite networking code
    rewrite networking code using cross-platform sockets library

---
+++ 360_stitcher/netlib.h
@@ -0,0 +1,221 @@
+////////////////////////////////////////////////////////////////////////////////
+/*
+sts_net.h - v0.07 - public domain
+written 2017 by Sebastian Steinhauer
+
+LICENSE
+Public domain. See "unlicense" statement at the end of this file.
+
+ABOUT
+A simple BSD socket wrapper.
+
+REMARKS
+The packet API is still work in progress.
+
+*/
+////////////////////////////////////////////////////////////////////////////////
+#ifndef __INCLUDED__STS_NET_H__
+#define __INCLUDED__STS_NET_H__
+
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+#ifndef STS_NET_SET_SOCKETS
+// define a bigger default if needed
+// this is the maximum amount of sockets you can keep in a socket set
+#define STS_NET_SET_SOCKETS   32
+#endif // STS_NET_SET_SOCKETS
+
+#ifndef STS_NET_BACKLOG
+// amount of waiting connections for a server socket
+#define STS_NET_BACKLOG       2
+#endif // STS_NET_BACKLOG
+
+#ifndef STS_NET_NO_PACKETS
+#ifndef STS_NET_PACKET_SIZE
+// the biggest possible size for a packet
+// note, that this size is already bigger then any MTU
+#define STS_NET_PACKET_SIZE   2048
+#endif // STS_NET_PACKET_SIZE
+#endif // STS_NET_NO_PACKETS
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//    Structures
+//
+typedef struct {
+	int   fd;             // socket file descriptor
+	int   ready;          // flag if this socket is ready or not
+	int   server;         // flag indicating if it is a server socket
+#ifndef STS_NET_NO_PACKETS
+	int   received;       // number of bytes currently received
+	int   packet_length;  // the packet size which is requested (-1 if it is still receiving the first 2 bytes)
+	char  data[STS_NET_PACKET_SIZE];  // buffer for the incoming packet
+#endif // STS_NET_NO_PACKETS
+} sts_net_socket_t;
+
+
+typedef struct {
+	sts_net_socket_t *sockets[STS_NET_SET_SOCKETS];
+} sts_net_set_t;
+
+
+// REMARK: most functions return 0 on success and -1 on error. You can get a more verbose error message
+// from sts_net_get_last_error. Functions which behave differently are the sts_net packet api and sts_net_check_socket_set.
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//    General API
+//
+// Get the last error from sts_net (can be called even before sts_net_init)
+const char *sts_net_get_last_error();
+
+// Initialized the sts_net library. You have to call this before any other function (except sts_net_get_last_error)
+int sts_net_init();
+
+// Shutdown the sts_net library.
+void sts_net_shutdown();
+
+
+////////////////////////////////////////////////////////////////////////////////
+//
+//    Low-Level Socket API
+//
+// Reset a socket (clears the structure).
+// THIS WILL NOT CLOSE the socket. It's ment to "clear" the socket structure.
+void sts_net_reset_socket(sts_net_socket_t *socket);
+
+// Check if the socket structure contains a "valid" socket.
+int sts_net_is_socket_valid(sts_net_socket_t *socket);
+
+// Open a (TCP) socket. If you provide "host" sts_net will try to connect to a remove host.
+// Pass NULL for host and you'll have a server socket.
+int sts_net_open_socket(sts_net_socket_t *socket, const char *host, const char *service);
+
+// Closes the socket.
+void sts_net_close_socket(sts_net_socket_t *socket);
+
+// Try to accept a connection from the given server socket.
+int sts_net_accept_socket(sts_net_socket_t *listen_socket, sts_net_socket_t *remote_socket);
+
+// Send data to the socket.
+int sts_net_send(sts_net_socket_t *socket, const void *data, int length);
+
+// Receive data from the socket.
+// NOTE: this call will block if the socket is not ready (meaning there's no data to receive).
+int sts_net_recv(sts_net_socket_t *socket, void *data, int length);
+
+// Initialized a socket set.
+void sts_net_init_socket_set(sts_net_set_t *set);
+
+// Add a socket to the socket set.
+int sts_net_add_socket_to_set(sts_net_socket_t *socket, sts_net_set_t *set);
+
+// Remove a socket from the socket set. You have to remove the socket from a set manually.
+// sts_net_close_socket WILL NOT DO THAT!
+int sts_net_remove_socket_from_set(sts_net_socket_t *socket, sts_net_set_t *set);
+
+// Checks for activity on all sockets in the given socket set. If you want to peek for events
+// pass 0.0f to the timeout.
+// All sockets will have set the ready property to non-zero if you can read data from it,
+// or can accept connections.
+//  returns:
+//    -1  on errors
+//     0  if there was no activity
+//    >0  amount of sockets with activity
+int sts_net_check_socket_set(sts_net_set_t *set, const float timeout);
+
+
+#ifndef STS_NET_NO_PACKETS
+// try to "refill" the internal packet buffer with data
+// note that the socket has to be "ready" so use it in conjunction with a socket set
+// returns:
+//  -1  on errors
+//   0  if there was no data
+//   1  added some bytes of new packet data
+int sts_net_refill_packet_data(sts_net_socket_t *socket);
+
+// tries to "decode" the next packet in the stream
+// returns 0 when there's no packet read, non-zero if you can use socket->data and socket->packet_length
+int sts_net_receive_packet(sts_net_socket_t *socket);
+
+// drops the packet after you used it
+void sts_net_drop_packet(sts_net_socket_t *socket);
+#endif // STS_NET_NO_PACKETS
+#endif // __INCLUDED__STS_NET_H__
+
+
+////////////////////////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////////////////////////
+////
+////    IMPLEMENTATION
+////
+////
+
+//// On Windows 64-bit, almost all socket functions use the type SOCKET
+//// to operate, but it's safe to cast it down to int, because handles
+//// can't get bigger then 2^24 on Windows...so I don't know why SOCKET is 2^64 ;)
+//// https://msdn.microsoft.com/en-us/library/ms724485(VS.85).aspx
+
+#include <string.h>   // NULL and possibly memcpy, memset
+
+#ifdef _WIN32
+#include <WinSock2.h>
+#include <Ws2tcpip.h>
+typedef int socklen_t;
+#pragma comment(lib, "Ws2_32.lib")
+#else
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <sys/select.h>
+#include <unistd.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#define INVALID_SOCKET    -1
+#define SOCKET_ERROR      -1
+#define closesocket(fd)   close(fd)
+#endif
+
+
+#ifndef sts__memcpy
+#define sts__memcpy     memcpy
+#endif // sts__memcpy
+#ifndef sts__memset
+#define sts__memset     memset
+#endif // sts__memset
+
+static const char *sts_net__error_message = "";
+
+#ifdef __cplusplus
+}
+#endif
+/*
+This is free and unencumbered software released into the public domain.
+
+Anyone is free to copy, modify, publish, use, compile, sell, or
+distribute this software, either in source code form or as a compiled
+binary, for any purpose, commercial or non-commercial, and by any
+means.
+
+In jurisdictions that recognize copyright laws, the author or authors
+of this software dedicate any and all copyright interest in the
+software to the public domain. We make this dedication for the benefit
+of the public at large and to the detriment of our heirs and
+successors. We intend this dedication to be an overt act of
+relinquishment in perpetuity of all present and future rights to this
+software under copyright law.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+OTHER DEALINGS IN THE SOFTWARE.
+
+For more information, please refer to <http://unlicense.org/>
+*/
--- 360_stitcher/networking.cpp
+++ 360_stitcher/networking.cpp
@@ -1,262 +1,107 @@
 #include "networking.h"
 #include "defs.h"
-#ifndef LINUX
-#include <WS2tcpip.h>
-#include <windows.h>
-#include <WinSock2.h>
-#else
-#include <unistd.h>
-#include <sys/types.h>
-#include <sys/socket.h>
-#include <netinet/in.h>
-#endif
+#include "netlib.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <opencv2/imgproc.hpp>
 #include <thread>
+#include <iostream>
+#include <fcntl.h>
 
+static const int MAX_CLIENTS = 10;
+static const int DEFAULT_BUFLEN = 10;
 
-#ifndef LINUX
-void pollFrames(SOCKET ConnectSocket, BlockingQueue<cv::Mat> &queue);
+/* initialized in startPolling() */
+static sts_net_socket_t server_socket;
 
-void pollClients(SOCKET ListenSocket, std::vector<BlockingQueue<cv::Mat>> &queue);
-#else
-void pollFrames(int ConnectSocket, BlockingQueue<cv::Mat> &queue);
-
-void pollClients(int ListenSocket, struct sockaddr_in &cli_addr, std::vector<BlockingQueue<cv::Mat>> &queue);
-#endif
-
-
-int startPolling(std::vector<BlockingQueue<cv::Mat>> &queues)
+static void pollFrames(sts_net_socket_t *socket, BlockingQueue<cv::Mat> &queue)
 {
-#ifdef LINUX
-    int sockfd, newsockfd, portno = std::stoi(CAPTURE_TCP_PORT);
-    socklen_t clilen;
-    /* char buffer[DEFAULT_BUFLEN]; */
-    struct sockaddr_in serv_addr, cli_addr;
-    int n;
+    int max_idx = CAPTURE_IMG_HEIGHT * CAPTURE_IMG_WIDTH * CAPTURE_IMG_CHANNELS;
+    char recvbuf[DEFAULT_BUFLEN];
+    int copy_length, overflow, index, error_count;
 
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) {
-        printf("ERROR opening socket\n");
-        return 1;
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
+    cv::Mat mat(cv::Size(CAPTURE_IMG_WIDTH, CAPTURE_IMG_HEIGHT), CV_MAKETYPE(CV_8U, CAPTURE_IMG_CHANNELS));
+    index = error_count = 0;
 
-    serv_addr.sin_family = AF_INET;
-    serv_addr.sin_addr.s_addr = INADDR_ANY;
-    serv_addr.sin_port = htons(portno);
+    /* Receive until peer closes the connection */
+    do {
+        memset(recvbuf, 0, DEFAULT_BUFLEN);
+        int bytes = sts_net_recv(socket, recvbuf, sizeof(recvbuf) - 1);
 
-    if (bind(sockfd, (struct sockaddr *) &serv_addr,
-             sizeof(serv_addr)) < 0) {
-        printf("ERROR on binding\n");
-        return 1;
-    }
-
-    listen(sockfd, 5);
-    std::thread th(pollClients, sockfd, std::ref(cli_addr), std::ref(queues));
-    th.detach();
-#else
-    WSADATA wsaData;
-    int iResult;
-
-    SOCKET ListenSocket = INVALID_SOCKET;
-
-    struct addrinfo *result = NULL;
-    struct addrinfo hints;
-
-    // Initialize Winsock
-    iResult = WSAStartup(MAKEWORD(2, 2), &wsaData);
-    if (iResult != 0) {
-        printf("WSAStartup failed with error: %d\n", iResult);
-        return 1;
-    }
-
-    ZeroMemory(&hints, sizeof(hints));
-    hints.ai_family = AF_INET;
-    hints.ai_socktype = SOCK_STREAM;
-    hints.ai_protocol = IPPROTO_TCP;
-    hints.ai_flags = AI_PASSIVE;
-
-    // Resolve the server address and port
-    iResult = getaddrinfo(NULL, CAPTURE_TCP_PORT, &hints, &result);
-    if (iResult != 0) {
-        printf("getaddrinfo failed with error: %d\n", iResult);
-        WSACleanup();
-        return 1;
-    }
-
-    // Create a SOCKET for connecting to server
-    ListenSocket = socket(result->ai_family, result->ai_socktype, result->ai_protocol);
-    if (ListenSocket == INVALID_SOCKET) {
-        printf("socket failed with error: %ld\n", WSAGetLastError());
-        freeaddrinfo(result);
-        WSACleanup();
-        return 1;
-    }
-
-    // Setup the TCP listening socket
-    iResult = bind(ListenSocket, result->ai_addr, (int)result->ai_addrlen);
-    if (iResult == SOCKET_ERROR) {
-        printf("bind failed with error: %d\n", WSAGetLastError());
-        freeaddrinfo(result);
-        closesocket(ListenSocket);
-        WSACleanup();
-        return 1;
-    }
-
-    freeaddrinfo(result);
-
-    iResult = listen(ListenSocket, SOMAXCONN);
-    if (iResult == SOCKET_ERROR) {
-        printf("listen failed with error: %d\n", WSAGetLastError());
-        closesocket(ListenSocket);
-        WSACleanup();
-        return 1;
-    }
-
-	std::thread th = std::thread(pollClients, ListenSocket, std::ref(queues));
-    th.detach();
-#endif
-	return 0;
-}
-
-
-
-#ifdef LINUX
-void pollFrames(int ConnectSocket, BlockingQueue<cv::Mat> &queue)
-{
-	int max_idx = CAPTURE_IMG_HEIGHT * CAPTURE_IMG_WIDTH * CAPTURE_IMG_CHANNELS;
-	int iResult;
-	const int recvbuflen = 128;
-	char recvbuf[recvbuflen];
-	int copy_length;
-	int overflow;
-
-	cv::Mat mat(cv::Size(CAPTURE_IMG_WIDTH, CAPTURE_IMG_HEIGHT), CV_MAKETYPE(CV_8U, CAPTURE_IMG_CHANNELS));
-	int index = 0;
-	// Receive until the peer closes the connection
-	do {
-        bzero(recvbuf, sizeof(recvbuf));
-        iResult = read(ConnectSocket, recvbuf, recvbuflen);
-		if (iResult > 0) {
-			//printf("Bytes received: %d\n", iResult);
-			copy_length = cv::min(iResult, max_idx - index);
-			memcpy(mat.data + index, recvbuf, copy_length);
-			index += iResult;
-		}
-		else if (iResult == 0) {
-			printf("Connection closed\n");
+        if (bytes < 0) {
+            LOGLN("ERROR: recv failed");
+            if (++error_count > 3) {
+                LOGLN("ERROR: maximum number of errors reached!");
+                break;
+            }
+        } else if (bytes == 0) {
+            LOGLN("connection was closed");
             break;
-		}
-		else {
-			printf("recv failed\n");
-		}
+        } else {
+            copy_length = cv::min(bytes, max_idx - index);
+            memcpy(mat.data + index, recvbuf, copy_length);
+            index += bytes;
+        }
 
-		if (index >= CAPTURE_IMG_WIDTH * CAPTURE_IMG_HEIGHT * CAPTURE_IMG_CHANNELS) {
-			index = 0;
-			cv::cvtColor(mat, mat, CV_YUV2BGR_NV12);
-			queue.push(mat);
+        if (index >= CAPTURE_IMG_WIDTH * CAPTURE_IMG_HEIGHT * CAPTURE_IMG_CHANNELS) {
+            index = 0;
+            cv::cvtColor(mat, mat, CV_YUV2BGR_NV12);
+            queue.push(mat);
 
-			mat = cv::Mat(cv::Size(CAPTURE_IMG_WIDTH, CAPTURE_IMG_HEIGHT), CV_MAKETYPE(CV_8U, CAPTURE_IMG_CHANNELS));
-			overflow = iResult - copy_length;
-			if (overflow) {
-				memcpy(mat.data, recvbuf + copy_length, overflow);
-				index = overflow;
-			}
-		}
-	} while (1);
+            mat = cv::Mat(cv::Size(CAPTURE_IMG_WIDTH, CAPTURE_IMG_HEIGHT), CV_MAKETYPE(CV_8U, CAPTURE_IMG_CHANNELS));
+            overflow = bytes - copy_length;
 
-    close(ConnectSocket);
+            if (overflow) {
+                memcpy(mat.data, recvbuf + copy_length, overflow);
+                index = overflow;
+            }
+        }
+    } while (1);
+
+    sts_net_close_socket(socket);
+    sts_net_shutdown();
 }
 
-void pollClients(int ListenSocket, struct sockaddr_in &cli_addr, std::vector<BlockingQueue<cv::Mat>> &queues)
+static void pollClients(sts_net_socket_t *socket, std::vector<BlockingQueue<cv::Mat>> &queue)
 {
-    std::cout << "Listening" << std::endl;
-    int idx;
+    int ptr = 0;
     int ConnectSocket;
-    socklen_t clilen = sizeof(cli_addr);
-    while(1) {
-        ConnectSocket = accept(ListenSocket, (struct sockaddr *) &cli_addr, &clilen);
-        if(ConnectSocket < 0) {
-            printf("accept failed\n");
+    sts_net_socket_t clients[MAX_CLIENTS];
+
+    LOGLN("listening...");
+
+    while (1) {
+        if (sts_net_accept_socket(socket, &clients[ptr]) < 0) {
+            fprintf(stderr, "ERROR: accept failed: '%s'\n", sts_net_get_last_error());
+            continue;
         } else {
-            printf("Client connected!");
-            std::thread th(pollFrames, ConnectSocket, std::ref(queues[idx]));
+            LOGLN("client connected");
+            std::thread th(pollFrames, &clients[ptr], std::ref(queue[ptr]));
             th.detach();
-            ++idx;
-            if (idx == queues.size()) {
-                idx = 0;
+
+            if (++ptr == MAX_CLIENTS) {
+                while (1) {
+                    LOGLN("ERROR: maximum number of clients reached");
+                    std::this_thread::sleep_for(std::chrono::milliseconds(5000));
+                }
             }
         }
     }
-    close(ListenSocket);
-}
-#else
-void pollFrames(SOCKET ConnectSocket, BlockingQueue<cv::Mat> &queue)
-{
-	int iResult;
-	const int frame_total_bytes = CAPTURE_IMG_WIDTH * CAPTURE_IMG_HEIGHT * CAPTURE_IMG_CHANNELS;
-	cv::Mat bgr_frame;
-	cv::Mat mat(cv::Size(CAPTURE_IMG_WIDTH, CAPTURE_IMG_HEIGHT), CV_MAKETYPE(CV_8U, CAPTURE_IMG_CHANNELS));
-	char* const data_ptr = (char*)mat.data;
-	// Receive until the peer closes the connection
-	do {
-		//Using MSG_WAITALL causes recv to block until we have received the full frame
-		iResult = recv(ConnectSocket, data_ptr, frame_total_bytes, MSG_WAITALL);
-		if (iResult > 0) {
-			cv::cvtColor(mat, bgr_frame, CV_YUV2BGR_NV12);
-			if (clear_buffers) {
-				while (!queue.empty()) {
-					queue.pop();
-				}
-			}
-			queue.push(bgr_frame);
-			
-		}
-		else if (iResult == 0) {
-			printf("Connection closed\n");
-            break;
-		}
-		else {
-			printf("recv failed with error: %d\n", WSAGetLastError());
-		}
-	} while (1);
 
-	// cleanup
-	closesocket(ConnectSocket);
-	WSACleanup();
+    sts_net_close_socket(socket);
 }
 
-
-void pollClients(SOCKET ListenSocket, std::vector<BlockingQueue<cv::Mat>> &queues)
+int startPolling(std::vector<BlockingQueue<cv::Mat>> &queues)
 {
-    SOCKET ClientSocket;
-	struct sockaddr_in ClientAddr;
-	int AddrSize = sizeof(ClientAddr);
-    printf("Polling for clients.\n");
-	int ConnectedClients = 0;
-    while (1) {
-        // Accept a client socket
-        ClientSocket = accept(ListenSocket, (struct sockaddr*) &ClientAddr, &AddrSize);
-		//Get the last octet of client's ip address. This octet is used to define the queue index of the client, so the streams
-		//will always be in the same order
-		unsigned char lastOctet = ClientAddr.sin_addr.S_un.S_un_b.s_b4;
+    sts_net_init();
 
-        if (ClientSocket == INVALID_SOCKET) {
-            printf("accept failed with error: %d\n", WSAGetLastError());
-        }
-        else
-        {
-			LOGLN("Client connected!");
-
-            std::thread th(pollFrames, std::ref(ClientSocket), std::ref(queues[lastOctet - clientAddrStart]));
-            th.detach();
-			ConnectedClients++;
-			if (ConnectedClients == NUM_IMAGES) {
-				break;
-			}
-        }
+    if (sts_net_open_socket(&server_socket, NULL, CAPTURE_TCP_PORT) < 0) {
+        fprintf(stderr, "ERROR: failed to open server socket: %s\n", sts_net_get_last_error());
+        return 1;
     }
+
+    std::thread th = std::thread(pollClients, &server_socket, std::ref(queues));
+    th.detach();
+
+    return 0;
 }
-#endif
